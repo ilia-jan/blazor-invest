99f9068020be19f5dcc3c1e17b247c7cf7ccb369
Ilia-Jan <i.lazarev.se@gmail.com>
2026-04-11 06:31:57 +0300

    some TODOs done

---
+++ src/BlazorInvest.Web/Extensions/DateExtension.cs
@@ -0,0 +1,24 @@
+﻿namespace BlazorInvest.Web.Extensions;
+
+public static class DateExtensions
+{
+    extension(DateTime date)
+    {
+        public string GetMonthName() => date.Month switch
+        {
+            1 => "Январь",
+            2 => "Февраль",
+            3 => "Март",
+            4 => "Апрель",
+            5 => "Май",
+            6 => "Июнь",
+            7 => "Июль",
+            8 => "Август",
+            9 => "Сентябрь",
+            10 => "Октябрь",
+            11 => "Ноябрь",
+            12 => "Декабрь",
+            _ => throw new ArgumentOutOfRangeException(nameof(date), "Месяц должен быть от 1 до 12")
+        };
+    }
+}
