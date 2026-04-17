5248d76608ff12ee6498aa0d04ca9329914e3f13
Ilia-Jan <i.lazarev.se@gmail.com>
2026-04-17 13:35:02 +0300

    proxy settings

--- src/BlazorInvest.Web/Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
+++ src/BlazorInvest.Web/Infrastructure/Extensions/WebApplicationBuilderExtensions.cs
@@ -10,14 +10,22 @@ public static class WebApplicationBuilderExtensions
     public static void RegisterServices(this WebApplicationBuilder builder)
     {
         builder.Services.Configure<InvestApiSettings>(builder.Configuration.GetSection("InvestApi"));
-        builder.Services.AddInvestApiClient("InvestApiClient", (services, settings) =>
-        {
-            var apiSettings = services.GetRequiredService<IOptions<InvestApiSettings>>().Value;
+        builder.Services
+            .AddInvestApiClient("InvestApiClient", (services, settings) =>
+            {
+                var apiSettings = services.GetRequiredService<IOptions<InvestApiSettings>>().Value;
 
-            settings.AccessToken = apiSettings.AccessToken;
-            settings.Sandbox = apiSettings.Sandbox;
-            settings.AppName = apiSettings.AppName;
-        });
+                settings.AccessToken = apiSettings.AccessToken;
+                settings.Sandbox = apiSettings.Sandbox;
+                settings.AppName = apiSettings.AppName;
+            })
+            .ConfigureHttpClientDefaults(b =>
+            {
+                b.ConfigurePrimaryHttpMessageHandler(() => new HttpClientHandler
+                {
+                    UseProxy = false
+                });
+            });
 
         builder.Services.AddScoped<ICouponsService, CouponsService>();
     }
